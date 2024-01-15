2b78c524824d46e5bdc5aaa91817bfeaf6990c3d
kartikeshtaware <84774219+kartikeshtaware@users.noreply.github.com>
2024-01-15 22:55:24 +0530

    Update operations.java
    subtraction function comments are added

--- operations.java
+++ operations.java
@@ -9,15 +9,15 @@ class operations
    
    }
    
-   static int sub(int a , int b)  
+   static int sub(int a , int b)  // this is subtraction function
    {
-      return a-b;                   
+      return a-b;      // subtraction function return value               
    
    }
    public static void main(String args[])
    {
-      System.out.println(add(20,30));   // function is called
-      System.out.println(sub(30,20)); 
+      System.out.println(add(20,30));   // addition function is called
+      System.out.println(sub(30,20));   // subtraction function is called
    
    }
 
