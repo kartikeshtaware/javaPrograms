7a57a91e4e65c8d117d2fefb41baf96137535eec
Kartikesh <2023bit504@sggs.ac.in>
2024-01-15 22:52:05 +0530

    subtraction function is added in code

--- operations.java
+++ operations.java
@@ -8,9 +8,16 @@ class operations
       return a+b;  // addition function return value                  
    
    }
+   
+   static int sub(int a , int b)  
+   {
+      return a-b;                   
+   
+   }
    public static void main(String args[])
    {
       System.out.println(add(20,30));   // function is called
+      System.out.println(sub(30,20)); 
    
    }
 
