09b70006dc8588047a3663a8f5b063e8370cea49
kartikeshtaware <84774219+kartikeshtaware@users.noreply.github.com>
2024-01-15 22:47:42 +0530

    Update comments operations.java
    here i update the comments in the code

--- operations.java
+++ operations.java
@@ -3,14 +3,14 @@ import java.util.*;
 class operations
 {
 
-   static int add(int a , int b)
+   static int add(int a , int b)  // this is addition function
    {
-      return a+b;
+      return a+b;  // addition function return value                  
    
    }
    public static void main(String args[])
    {
-      System.out.println(add(20,30));
+      System.out.println(add(20,30));   // function is called
    
    }
 
