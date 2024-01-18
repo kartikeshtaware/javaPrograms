dbd81692803636cbf6496a1d34f81f4c7a733b2a
Kartikesh <2023bit504@sggs.ac.in>
2024-01-18 20:52:09 +0530

    1. input one string and one number and display 2. input 3 number and display

---
+++ Hi5Hacker.java
@@ -0,0 +1,28 @@
+/*ystem.out.println(String). So, if our input is:
+
+input
+Hi 5
+
+Our code will print:
+
+myString is: Hi
+myInt is: 5
+
+*/
+
+import java.util.*;
+
+class Hi5Hacker {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String b = scan.next();
+        int a = scan.nextInt();
+        
+
+        System.out.println("myString is: "+b);
+        System.out.println("myInt is: "+a);
+        // Complete this line
+        // Complete this line
+    }
+}
