c6bd1c83901636273253445ed43e6ca0c951a7de
Kartikesh <2023bit504@sggs.ac.in>
2024-01-21 23:57:46 +0530

    Make a input table

---
+++ table.java
@@ -0,0 +1,11 @@
+/*
+
+Given an integer, , print its first multiples. Each multiple (where ) should be printed on a new line in the form: N x i = result.
+
+*/
+
+import java.util.*;
+
+public class table
+{
+}
