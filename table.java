3fa0c204c9b2b6f96f04f248ce803a7cdcdcaa86
Kartikesh <2023bit504@sggs.ac.in>
2024-01-22 00:11:39 +0530

    Here user input is added

--- table.java
+++ table.java
@@ -1,6 +1,24 @@
 /*
 
-Given an integer, , print its first multiples. Each multiple (where ) should be printed on a new line in the form: N x i = result.
+Given an integer N , print its first 10 multiples. Each multiple N* i (where 1<=i<=10 ) should be printed on a new line in the form: N x i = result.
+
+Sample Input
+
+2
+
+Sample Output
+
+2 x 1 = 2
+2 x 2 = 4
+2 x 3 = 6
+2 x 4 = 8
+2 x 5 = 10
+2 x 6 = 12
+2 x 7 = 14
+2 x 8 = 16
+2 x 9 = 18
+2 x 10 = 20
+
 
 */
 
@@ -8,4 +26,18 @@ import java.util.*;
 
 public class table
 {
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+    int N = scan.nextInt();
+    int i;
+    for(i=1;i<=10;i++)
+    {
+      System.out.println(N+" x "+i+" = "+N*i );
+    
+    }
+    
+  }
+
+
 }
